8fd99e632ec856fcfa7d2b9d78d74a79ddede4b2
Tadeo-VC <tvazquez@frba.utn.edu.ar>
2026-02-02 16:50:21 -0300

    feat:added message entity

---
+++ src/domain/message.ts
@@ -0,0 +1,18 @@
+class Message {
+  private request: string;
+  private response?: string;
+  private responseState: ResponseState = ResponseState.IDLE;
+
+  constructor(request: string) {
+    this.request = request;
+  }
+
+  async sendMessage(agent: Agent){
+    this.response = await agent.receiveMessage(this);
+  }
+
+  receiveResponse(response: string){
+    this.response = response;
+    this.responseState = ResponseState.SUCCESS;
+  }
+}

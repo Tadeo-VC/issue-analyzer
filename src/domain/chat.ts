6581253cffda05d8559120faae88c7bda2d0589d
Tadeo-VC <tvazquez@frba.utn.edu.ar>
2026-02-02 16:49:04 -0300

    feat:added chat entity

---
+++ src/domain/chat.ts
@@ -0,0 +1,19 @@
+class Chat {
+  private title: string;
+  private messages: Message[];
+  private user: User;
+  private agent: Agent;
+
+  constructor(title: string, messages: Message[], user: User, agent: Agent) {
+    this.title = title;
+    this.messages = messages;
+    this.user = user;
+    this.agent = agent;
+  }
+
+  newMessage(request: string): Message {
+    const message = new Message(request);
+    this.messages.push(message);
+    return message;
+  }
+}

72e47e988988ac3edd4a2cc54cc0ed5e20a2ffda
Tadeo-VC <tvazquez@frba.utn.edu.ar>
2026-02-08 12:12:12 -0300

    refactor: update generateResponse to support multiple tool calls (partial)

--- src/domain/llm/clientLLM.ts
+++ src/domain/llm/clientLLM.ts
@@ -4,17 +4,57 @@ import { IntentData } from "./intentData";
 import { Intention, LLMMessage, LLMRole } from "./llmMessage";
 import { SystemPrompt } from "./prompts";
 import { UnsupportedIntentionError } from "../errors";
+import z from "zod";
 
 export abstract class ClientLLM {
 
-  async generateResponse(chat: Chat): Promise<GenerateResult> {
+  async generateResponse(chat: Chat){
 
-    const responseText: IntentData = await this.sendRequest(
-      this.buildPrompt(SystemPrompt.FIND_INTENTION),
+    const llmResponse: IntentData = await this.sendRequest(
+      this.buildPrompt(SystemPrompt.FIND_USER_INTENTIONS),
       this.buildChatHistory(chat),
       this.buildLastMessage(chat)
     );
+    
+    const zodLlmResponse = llmToolCallingSchema.safeParse([llmResponse]);
+    if (!zodLlmResponse.success) {
+      throw new ClientLLMException(`Invalid LLM response: ${zodLlmResponse.error.message}`);
+    }
+
+    const jsonLlmResponse = zodLlmResponse.data;
   
+    if (jsonLlmResponse.length === 0) {
+      throw new ClientLLMException("LLM response is empty");
+    }
+
+    if (jsonLlmResponse.length == 1 && jsonLlmResponse[0].intention === Intention.GENERAL_CHAT) {
+      // pasamos directamente la respuesta del llm al usuario, sin pasar por el sistema de herramientas
+    } 
+
+    return this.callTools(jsonLlmResponse, chat);
+  }
+
+  private async callTools(tools: LlmToolCalling, chat: Chat) {
+    
+  }
+}
+const llmToolCallingSchema = z.array(
+  z.object({
+    intention: z.enum([Intention.ANALYZE_ISSUES_COMPLEXITY, Intention.PERSIST_CHAT, Intention.GENERAL_CHAT]),
+    args: z.record(z.string(), z.unknown())
+  })
+);
+
+type LlmToolCalling = z.infer<typeof llmToolCallingSchema>;
+
+export class ClientLLMException extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "ClientLLMException";
+  }
+}
+
+/*
     let payload = {}
 
     switch (responseText.intention) {
@@ -91,3 +131,4 @@ export abstract class ClientLLM {
     return new LLMMessage(LLMRole.USER, lastMessage.getRequest());
   }
 }
+*/

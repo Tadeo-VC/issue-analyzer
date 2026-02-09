886418534bdc174b7473ab4de7a5a225e94d50e9
Tadeo-VC <tvazquez@frba.utn.edu.ar>
2026-02-09 02:50:00 -0300

    refactor: update chatFactory to accept clientLlm as a parameter

--- src/domain/factories/chatFactory.ts
+++ src/domain/factories/chatFactory.ts
@@ -1,13 +1,11 @@
 import { uuidv4 } from "zod/v4/mini";
 import { User } from "../user";
 import { Chat } from "../chat";
-import { Agent } from "../agent";
-import { AgentFactory } from "./agentFactory";
-
+import { issuesComplexityAnalyzerTool, OpenAILLM, persistChatTool } from "../llm/openAI";
 export class ChatFactory {
 
   static async createNewChat(title: string, user: User): Promise<Chat> {
-    const agent = await AgentFactory.createAgent()
-    return new Chat(title, [], user, agent,  uuidv4().toString()); // At runtime, Zod UUID behaves just like a string.
+
+    return new Chat(title, [], user, new OpenAILLM([issuesComplexityAnalyzerTool, persistChatTool]),  uuidv4().toString()); // At runtime, Zod UUID behaves just like a string.
   }
 }
